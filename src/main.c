c6057405d335e941f6f2996879bb9dd98009c34c
Liam Wynn <wynnliam@pdx.edu>
2019-07-03 13:49:07 -0700

    Removed unused variables in main.c

--- src/main.c
+++ src/main.c
@@ -5,35 +5,9 @@
 
 #include "loop_handling.h"
 
-// What we pass to the loop function
-struct context {
-	SDL_Renderer* renderer;
-};
-
-// The main loop function.
-void loop(void* args) {
-	struct context* ctx = (struct context*)args;
-	SDL_Renderer* renderer = ctx->renderer;
-
-	// Handle all non-rendering logic before we update.
-	// Things like keyboard input and user movement.
-	update();
-	// Do a ray-casting rendering step.
-	render(renderer);
-
-	//emscripten_cancel_main_loop();
-}
-
 int main() {
 	SDL_Window* window;
 	SDL_Renderer* renderer;
-
-	struct context ctx;
-	// Using -1 makes the renderer run as fast as possible.
-	const int fps_count = -1;
-	// If true, runs the loop infinitely.
-	const int run_infinite = 1;
-
 
 	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
 
@@ -41,8 +15,6 @@ int main() {
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
 
 	//SDL_CreateWindowAndRenderer(320, 200, 0, &window, &renderer);
-
-	ctx.renderer = renderer;
 
 	// Initialize any non-SDL logic
 	initialize(renderer);
