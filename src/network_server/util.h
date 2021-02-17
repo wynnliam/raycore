eb854fd06d5a9bb602296bd9972936d6f937bb1a
Liam Wynn <wynnliam@pdx.edu>
2021-02-17 15:43:12 -0800

    Added client_message type

--- src/network_server/util.h
+++ src/network_server/util.h
@@ -6,6 +6,12 @@
 #define MAX_CLIENTS		16
 #define PORT	20715
 
+// A single integer the server can send the client
+#define CLIENT_SIGNAL	0
+// The state of the game. TODO: Should recieve
+// as UDP
+#define CLIENT_GAME		CLIENT_SIGNAL + 1
+
 // This is what we will send to each client every
 // so often.
 typedef struct {
@@ -13,5 +19,13 @@ typedef struct {
   int level_id;
   int x, y, rot;
 } client_data;
+
+typedef struct {
+  char type;
+  struct union {
+    char signal;
+    client_data[MAX_CLIENTS] game;
+  } data; 
+} client_message;
 
 #endif
