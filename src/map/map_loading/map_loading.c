7b38e24a1fc54ac99f6ab24d773c4146b180c02c
Liam Wynn <wynnliam@pdx.edu>
2021-06-20 21:57:35 -0700

    current bitmap loader does not work

--- src/map/map_loading/map_loading.c
+++ src/map/map_loading/map_loading.c
@@ -241,9 +241,10 @@ static void create_wall_textures(struct texture_list* textures, struct mapdef* r
 				result->walls[walldef_index].surf = SDL_LoadBMP(result->walls[walldef_index].path);
 
         unsigned int tw, th;
-        loadbmp_decode_file(result->walls[walldef_index].path, &(result->walls[walldef_index].data), &tw, &th, LOADBMP_RGB);
+        loadbmp_decode_file(result->walls[walldef_index].path, &(result->walls[walldef_index].data), &tw, &th, LOADBMP_RGBA);
         result->walls[walldef_index].tw = tw;
         result->walls[walldef_index].th = th;
+        printf("Texture size: w = %d h = %d\n", tw, th);
 			} else {
 				result->walls[walldef_index].path = NULL;
 				result->walls[walldef_index].surf = NULL;
